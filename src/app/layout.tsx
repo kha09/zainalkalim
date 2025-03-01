87c9d819131e89371feea3ab8d51cc4e3b0b3fad
unknown <dev.khalid.0@gmail.com>
2025-03-01 12:00:54 +0300

    small changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Link from 'next/link'
-import Image from 'next/image'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,25 +17,8 @@ export default function RootLayout({
         <nav className="fixed top-0 left-0 w-full bg-white shadow-md z-50">
           <div className="container mx-auto px-4 py-3 flex justify-between items-center">
             <div className="flex items-center">
-              <Link href="/" className="flex items-center space-x-2 rtl:space-x-reverse pr-4">
-                <Image 
-                  src="/logos/Nu7aa Logo.svg" 
-                  alt="Logo" 
-                  width={50} 
-                  height={50} 
-                  className="rounded-full"
-                />
-                <span className="text-xl font-semibold text-gray-800"></span>
-              </Link>
-              <Link href="/" className="flex items-center space-x-2 rtl:space-x-reverse pr-6">
-                <Image 
-                  src="/logos/allam.svg" 
-                  alt="Logo" 
-                  width={50} 
-                  height={50} 
-                  className="rounded-full"
-                />
-                <span className="text-xl font-semibold text-gray-800"></span>
+              <Link href="/" className="flex items-center">
+                <h1 className="text-2xl font-bold text-purple-600">زين الكلم</h1>
               </Link>
             </div>
           </div>
